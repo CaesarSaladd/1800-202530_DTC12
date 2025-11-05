babc42dc50b32b56b501a0cc9ac5530bfeb93a32
Lyra <d.g.shorman@my.bcit.ca>
2025-11-04 16:00:51 -0800

    add Craves/leftovers logic

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 
+import { db } from "./firebaseConfig.js";
+import { doc, onSnapshot, getDoc, setDoc } from "firebase/firestore";
+
 function sayHello() {
 
 }
