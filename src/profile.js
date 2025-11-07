ac46255a174aa4be85bc9b09f3c545dabc50f283
JAMEEL MOHAMMED <jameeel01@gmail.com>
2025-11-06 18:20:10 -0800

    Add swiping and logout button

--- src/profile.js
+++ src/profile.js
@@ -1,4 +1,28 @@
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { db } from "./firebaseConfig.js";
-import { collection, getDocs, doc, deleteDoc } from "firebase/firestore";
+import { auth } from "./firebaseConfig.js";
+import { onAuthStateChanged } from "firebase/auth";
+import { logoutUser } from "./authentication.js";
 
+// Chekc if user logged in
+onAuthStateChanged(auth, (user) => {
+    if (!user) {
+        window.location.href = "login.html";
+        return;
+    }
+});
+
+// Logout button
+document.addEventListener("DOMContentLoaded", () => {
+    const logoutBtn = document.getElementById("logoutBtn");
+if (logoutBtn) {
+    logoutBtn.addEventListener("click", async () => {
+        try {
+            await logoutUser();
+            console.log("Logged out");
+        } 
+        catch (error) {
+        console.error("Logout error:", error);
+        alert("Logout failed. Please try again.");
+        }
+    });
+}
+});
